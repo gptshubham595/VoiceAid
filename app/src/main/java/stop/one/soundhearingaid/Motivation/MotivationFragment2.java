3d7f569a0df729dc407809a181029cc7908e5da3
Shubham gupta Ggps <gptshubham595@gmail.com>
2020-02-09 20:01:45 +0530

    motiv

---
+++ app/src/main/java/stop/one/soundhearingaid/Motivation/MotivationFragment2.java
@@ -0,0 +1,31 @@
+package stop.one.soundhearingaid.Motivation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+
+import stop.one.soundhearingaid.R;
+
+public class MotivationFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_motivation2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        int position = FragmentPagerItem.getPosition(getArguments());
+
+    }
+
+}
